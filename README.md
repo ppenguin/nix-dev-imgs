697e5a8ea550c513e985641f690bfd6a4197245c
ppenguin <hieronymusv@gmail.com>
2023-06-27 15:21:06 +0200

    improvements

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# nix-dev-imgs
-Nix flake with `devShell`s and corresponding docker image build expressions (images intended to use in CIs providing the same dev env as the `devShell`s)
+# SuDoF (SuperDevOpsFlake)
+
+## Rationale
+
+For developing, running and building software in a deterministic environment, we have come to know
+and love (optionally `direnv`-powered) `devShells` (from `flake` outputs), or `shell.nix` (the "former" way.)
+
+To achieve (almost) identical build environments in container images we use in CI/CD pipelines on (e.g. `gitlab`-) runners,
+it would be cool if we could automatically reproduce said `devShell` environment in a container image.
+
+> Now we can
+
+## Usage (`flake`)
+
+### The `devShell`
+
+To enter the `devShell`:
+
+```sh
+nix develop path/to/flake-dir
+# because this flake only defines one `devShell` attribute, no further specifier is necessary
+```
+
+or automatically by having a working `direnv` and put in your `.envrc`:
+
+```sh
+#!/usr/bin/env bash
+# .envrc
+...
+use flake path/to/flake-dir
+...
+```
+
+### Build and Push an Image
+
+To build a compressed container image archive to `/nix/store/...` (using `buildLayeredImage`):
+
+```sh
+nix build path/to/flake-dir#ansibleBLImg
+podman load </nix/store/compressed-image....tgz   # or docker
+```
+
+To build a script that outputs (streams) the uncompressed image to `stdout` when run:
+
+```sh
+nix build path/to/flake-dir#ansibleSLImg
+./result | podman load   # or docker
+```
+
+To build the image and upload it to our internal registry:
+
+```sh
+REGISTRY="docker://1nnoserv:15000" REPO="devops-imgs" nix run path/to/flake-dir#push2reg
+```
+
+or for a public registry (e.g.):
+
+```sh
+REGISTRY="quay.io" REGACCOUNT=reguser DOCKERHUBTOKEN=regpasswd REPO="devops-imgs" nix run path/to/flake-dir#push2reg
+# the target will be expanded to:
+# quay.io/reguser/devops-imgs/imageName:imageTags
+# where imageName and imageTags are set in the flake
+```
+
+## Limitations
+
+This setup currently is meant to be used as one `flake` per environment, which is in principle the "right way", since it
+addresses an environment related to a `git` repo.
+
+The first sensible improvement would be to wrap the derivation functions themselves in a generic way and outfactor them to a
+"public library `flake`" that can be used as an input. (This should be trivial.)
--- flake.nix
+++ flake.nix
@@ -15,16 +15,24 @@
 
       inherit (pkgs) lib;
 
-      py = pkgs.python310;
+      imageName = "ansible-omni-deploy"; # an "omni-potent" deploy image, i.e. contains ansible modules for systemd, database, podman etc. management
+      imageTags = "ansible2.15.0";
+
+      py = pkgs.python3; # TODO: find out why/by whom some python3.10 deps are pulled in if this is set to 3.11 (=> duplicates)
       # below implies python interpreter itself, do not separately specify!
-      pypkgs = [ (py.withPackages (p: with p; [ pexpect ansible-core jmespath psycopg2 ]))];
-      pyenvpaths = pypkgs ++ (with pkgs; [ ansible-lint ]);
+      pypkgs = [ (py.withPackages (p: with p; [ pexpect ansible-core /* ansible */ jmespath psycopg2 ]))];
+      pyenvpaths =
+        pypkgs ++
+        (with pkgs; [
+          (ansible-lint.override { python3=py; })
+        ]);
       pyenv = pkgs.buildEnv {
         name = "mypy";
         paths = pyenvpaths;
       };
 
-      ansibleCollectionPath = pkgs.callPackage ./ansible-collections.nix {} pkgs.ansible {
+      # we do pyenv instead of pkgs.ansible, because we need the same python version as for the rest of our env referenced
+      ansibleCollectionPath = pkgs.callPackage ./ansible-collections.nix {} pyenv {
         "containers-podman" = {
             version = "1.10.2";
             sha256 = "sha256:1g74pi0fslgbcp80710q42pfaifiai9hwaz69mi1bm8lqiz79ip8";
@@ -43,68 +51,64 @@
             usrBinEnv
             binSh
             caCertificates
-            fakeNss
+            # fakeNsss
           ]) ++
           (with pkgs; [
-            git jq gawk toybox
+            bash
+            git jq mawk toybox
           ]);
       });
 
-    in {
+    in rec {
 
       devShell = pkgs.mkShell {
-        buildInputs = [ pyenv ] ++ (with pkgs; [ dive ]);
-        ## Apparently we have to set the path explicitly to make it override other (global) python interpreters
+        buildInputs = [ pyenv ] ++ (with pkgs; [ dive nix ]);
         shellHook = ''
           export ANSIBLE_COLLECTIONS_PATHS="${ansibleCollectionPath}"
         '';
       };
 
-      packages = {
-
-        ansibleSLImg = pkgs.dockerTools.streamLayeredImage {
-          name = "ansible-omni-deploy"; # an "omni-potent" deploy image, i.e. contains ansible modules for systemd, database, podman etc. management
-          tag = "latest";
-
-          contents = imgContentDrv;
-
-          config = {
-            Cmd = [
-              "ansible-playbook" "--version"
-            ];
-          };
+      apps = {
+        push2reg = {
+          type = "app";
+          program = let scriptname="push2reg"; in ''${(with pkgs; writeShellScriptBin scriptname ''
+            if [ -z "$REGISTRY" ] || [ -z "$REPO" ]; then
+              echo "Env var REGISTRY and REPO required!"
+              exit 1
+            fi
+            if [ -n "$REGACCOUNT" ] && [ -n "$DOCKERHUBTOKEN" ]; then
+              echo "Logging in to the remote REPO=$REPO with user REGACCOUNT=$REGACCOUNT and the password in env var DOCKERHUBTOKEN"
+              podman login -u "$REGACCOUNT" -p "$DOCKERHUBTOKEN" "$REPO"
+            else
+              echo "No credentials REGACCOUNT, DOCKERHUBTOKEN given, not logged in"
+            fi
+            REGACCOUNT=''${REGACCOUNT:+$REGACCOUNT/}
+            podman load < ${self.outputs.packages.${system}.ansibleBLImg.out} && podman push ${imageName}:${imageTags} "$REGISTRY/$REGACCOUNT$REPO/${imageName}:${imageTags}"
+          '')}/bin/${scriptname}'';
         };
-
-        ansibleBLImg = pkgs.dockerTools.buildLayeredImage {
-          name = "ansible-omni-deploy"; # an "omni-potent" deploy image, i.e. contains ansible modules for systemd, database, podman etc. management
-          tag = "latest";
-
-          contents = imgContentDrv;
-
-          config = {
-            Cmd = [
-              "ansible-playbook" "--version"
-            ];
-          };
-        };
-
-        ansibleImg = pkgs.dockerTools.buildImage {
-          name = "ansible-omni-deploy"; # an "omni-potent" deploy image, i.e. contains ansible modules for systemd, database, podman etc. management
-          tag = "latest";
-
-          copyToRoot = imgContentDrv;
-          extraCommands = ''
-          '';
-
-          config = {
-            Cmd = [
-              "ansible-playbook" "--version"
-            ];
-          };
-        };
-
       };
 
+      packages =
+      let
+        imgdef = {
+          name = imageName;
+          tag = imageTags;
+          created = "now";
+          contents = imgContentDrv;
+          config = {
+            Cmd = [
+              "ansible-playbook" "--version"
+            ];
+            Env = [
+              # "ANSIBLE_PYTHON_INTERPRETER=/bin/python3"
+              "ANSIBLE_COLLECTIONS_PATHS=${ansibleCollectionPath}"
+            ];
+          };
+        };
+      in {
+        ansibleSLImg = pkgs.dockerTools.streamLayeredImage imgdef;
+        ansibleBLImg = pkgs.dockerTools.buildLayeredImage imgdef; # !!! LayeredImage doesn't have the bug where copies instead of links are made to the image root
+      };
     }
   );
 }
