783c9ff85db095de0a4d6cc7418e140e80d46617
ppenguin <hieronymusv@gmail.com>
2024-09-03 18:32:00 +0200

    add qemu to bind image (for multi-arch images)

--- devenvs/bind/imgdrv.nix
+++ devenvs/bind/imgdrv.nix
@@ -1,65 +1,75 @@
-{ name
-, pkgs
-,
-}:
-let
-  inherit (import ./build-inputs.nix { inherit pkgs; }) commonInputs;
-  inherit (import ./_contfiles.nix { inherit pkgs; })
-    containersconf storageconf registriesconf policyjson passwd group nsswitchconf subuid subgid;
+{
+  name,
+  pkgs,
+}: let
+  inherit (import ./build-inputs.nix {inherit pkgs;}) commonInputs;
+  inherit
+    (import ./_contfiles.nix {inherit pkgs;})
+    containersconf
+    storageconf
+    registriesconf
+    policyjson
+    passwd
+    group
+    nsswitchconf
+    subuid
+    subgid
+    ;
 in
-# https://ryantm.github.io/nixpkgs/builders/images/dockertools/
-pkgs.dockerTools.buildLayeredImage {
-  inherit name;
-  tag = "1.0.0";
-  created = "now";
-  contents = pkgs.buildEnv {
-    name = "image-root";
-    pathsToLink = [ "/usr" "/bin" "/var" "/etc" ];
-    paths =
-      [
-        containersconf
-        storageconf
-        registriesconf
-        policyjson
-        subuid
-        subgid
-        passwd
-        group
-        nsswitchconf
-      ]
-      ++ commonInputs
-      ++ (with pkgs.dockerTools; [
-        # "minimal" container env
-        usrBinEnv
-        binSh # https://ryantm.github.io/nixpkgs/builders/images/dockertools/#sssec-pkgs-dockerTools-helpers-binSh
-        caCertificates
-        # fakeNss # this is for /etc/{passwd,group,nsswitch.conf} (what else???)
-      ])
-      ++ (with pkgs; [
-        # "minimal" shell env with some dev tools
-        shadow # for newuidmap etc.
-        bashInteractive # already have binSh, but this gives us the link /bin/bash
-        binutils # for strip
-        coreutils
-        fuse-overlayfs # for running builder in a container
-        git
-        gnutar
-        gzip
-        gawk
-        gnused
-        jq # needed for our g1tlab package makefiles for API access
-        which
-      ]);
-  };
-  config = {
-    Cmd = [
-      "buildah"
-      "--version"
-    ];
-    Env = [
-      "TEMP=/var/tmp"
-    ];
-    uid = 1000;
-    gid = 100;
-  };
-}
+  # https://ryantm.github.io/nixpkgs/builders/images/dockertools/
+  pkgs.dockerTools.buildLayeredImage {
+    inherit name;
+    tag = "1.1.0";
+    created = "now";
+    contents = pkgs.buildEnv {
+      name = "image-root";
+      pathsToLink = ["/usr" "/bin" "/var" "/etc"];
+      paths =
+        [
+          containersconf
+          storageconf
+          registriesconf
+          policyjson
+          subuid
+          subgid
+          passwd
+          group
+          nsswitchconf
+        ]
+        ++ commonInputs
+        ++ (with pkgs.dockerTools; [
+          # "minimal" container env
+          usrBinEnv
+          binSh # https://ryantm.github.io/nixpkgs/builders/images/dockertools/#sssec-pkgs-dockerTools-helpers-binSh
+          caCertificates
+          # fakeNss # this is for /etc/{passwd,group,nsswitch.conf} (what else???)
+        ])
+        ++ (with pkgs; [
+          # "minimal" shell env with some dev tools
+          shadow # for newuidmap etc.
+          bashInteractive # already have binSh, but this gives us the link /bin/bash
+          binutils # for strip
+          coreutils
+          fuse-overlayfs # for running builder in a container
+          git
+          gnutar
+          gzip
+          gawk
+          gnused
+          jq # needed for our g1tlab package makefiles for API access
+          which
+          qemu # now we (hopefully) have multi-arch capability (requires --privileged, binfmt mounted and configured on runner)
+        ]);
+    };
+    config = {
+      Cmd = [
+        "buildah"
+        "--version"
+      ];
+      Env = [
+        "TEMP=/var/tmp"
+      ];
+      uid = 1000;
+      gid = 100;
+    };
+  }
